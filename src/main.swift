005a087d80f19cab24b171b9ee381d9477eaca0d
Detours <dev@detours.app>
2026-06-25 08:01:20 +0200

    Handle filter shortcut during app event dispatch

--- src/main.swift
+++ src/main.swift
@@ -1,6 +1,18 @@
 import AppKit
 
-let app = NSApplication.shared
+final class DetoursApplication: NSApplication {
+    override func sendEvent(_ event: NSEvent) {
+        if event.type == .keyDown,
+           ShortcutManager.shared.matches(event: event, action: .filter) {
+            (delegate as? AppDelegate)?.filter(nil)
+            return
+        }
+
+        super.sendEvent(event)
+    }
+}
+
+let app = DetoursApplication.shared
 app.setActivationPolicy(.regular)
 let delegate = AppDelegate()
 app.delegate = delegate
